0a13875082536093cf0012cb7d4834c4fd1596f5
Ameya Barsode <abarsode@yahoo-inc.com>
2014-04-24 03:28:42 -0700

    Build Course details page.

--- apps/MobileMooc/android/native/assets/www/default/worklight/checksum.js
+++ apps/MobileMooc/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1253310665,"date":1398233323006,"machine":"likemillion-lm"};
-/* Date: Tue Apr 22 23:08:43 PDT 2014 */
+var WL_CHECKSUM = {"checksum":3835500027,"date":1398322961041,"machine":"likemillion-lm"};
+/* Date: Thu Apr 24 00:02:41 PDT 2014 */
